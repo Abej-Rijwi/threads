62b717d23147efcb71fa9c4590c4d8a3d102ac1c
Abej-Rijwi <abejrijwicareers@gmail.com>
2025-03-28 02:00:15 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isIgnoredRoute = createRouteMatcher(["/api/webhook/clerk"]);
 
 export default clerkMiddleware(
   async (auth, request) => {
-    PublicRoutes: ["/", "/api/webhook/clerk"];
-    ignoredRoutes: ["/api/webhook/clerk"];
+    if (isIgnoredRoute(request)) {
+      return;
+    }
 
     if (!isPublicRoute(request)) {
       await auth.protect();
